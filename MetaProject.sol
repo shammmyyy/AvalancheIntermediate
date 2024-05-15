5171f113c9284c0e3f217df661757d361bf03000
shammmyyy <129077959+shammmyyy@users.noreply.github.com>
2024-05-15 09:32:36 +0800

    MetaProject.sol

--- MetaProject.sol
+++ MetaProject.sol
@@ -1,18 +1,53 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
 
-contract project {
-    uint public display;
+contract FunctionAndError {
+    uint Assert;
+    uint Require;
+    uint Revert;
+    uint public DefaultValue ;
+    address public Admin;
+ 
+    constructor(address MainAdmin, uint _DefaultValue){
+        DefaultValue = _DefaultValue;
+        Admin = MainAdmin;
+    }
 
-    function get(uint _set) external {
-        
-        require(_set != 10, "New value cannot be 10");
-        assert(_set != 5); 
-        
-        if (_set == 3) {
-            revert("New value cannot be 3");
+    modifier _MainAdmin{
+        require (Admin == msg.sender, "unlocate address please try again!");
+        _;
+    }
+
+     function SetUpValue(uint _Assert, uint _Require, uint _Revert) public _MainAdmin{
+        Assert = _Assert;
+        Require = _Require;
+        Revert = _Revert;
+    }
+
+    function Asserted() public view _MainAdmin {
+       if(Assert == 0){
+            revert("Please Enter a value");
+       }else{
+            assert(Assert != DefaultValue);
+       }
+    }
+
+    function Required() public view _MainAdmin {
+        if(Require == 0){
+            revert("Please Enter a value");
+       }else{
+            require(Require <= DefaultValue, "Required Value is cannot be used, try another value");
+       }
+    }
+
+    function Reverted() public view _MainAdmin {
+        if(Require == 0){
+            revert("Please Enter a value");
+        }else if(Revert <= DefaultValue){
+            revert("Revert Value is cannot be used, try another value");
+        }else{
+
         }
-
-        display = _set;
     }
 }
+
